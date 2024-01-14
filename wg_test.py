9262bdc5147da58c5394b9d3ef45ad5e14f490ce
jizhilong <zhilongji@gmail.com>
2024-01-14 16:08:01 +0800

    test: add a simple integration test

--- wg_test.py
+++ wg_test.py
@@ -1,17 +1,101 @@
 import random
+import threading
 import timeit
+
+import pytest as pytest
+from scapy.layers.inet import raw, IP
+
 from wg import *
 
 
-def test_handshake_func():
-    i_private, i_public = dh_generate()
-    r_private, r_public = dh_generate()
-    i_node, r_node = WgNode('', '', 0, i_private, i_public, []), WgNode('', '', 0, r_private, r_public, [])
-    handshake = i_node.init_handshake(r_public)
-    si_pub, timestamp, r_pair, resp_msg = r_node.respond_to_handshake(handshake.req)
+@pytest.fixture
+def wg_keys():
+    return dh_generate(), dh_generate()
+
+
+@pytest.fixture
+def wg_ports():
+    ports = []
+    for i in range(2):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(('127.0.0.1', 0))
+        port = sock.getsockname()[1]
+        ports.append(port)
+        sock.close()
+    return tuple(ports)
+
+
+@pytest.fixture(autouse=True)
+def mock_tun_setup(monkeypatch):
+    def mock_setup_tun(self):
+        pass
+    monkeypatch.setattr(WgNode, '_setup_tun_dev', mock_setup_tun)
+
+
+@pytest.fixture
+def wg1(wg_keys, wg_ports):
+    (private_1, public_1), (private_2, public_2) = wg_keys
+    (port_1, port_2) = wg_ports
+    node = WgNode(ip='10.0.0.1', listen_ip='127.0.0.1', listen_port=port_1,
+                  private=private_1, public=public_1, peers=[])
+    node.peers.append(WgPeer('10.0.0.2', ('127.0.0.1', port_2), public_2, node))
+    s1, s2 = socket.socketpair(type=socket.SOCK_DGRAM)
+    node.tun = s1.fileno()
+    node._tun = s1
+    node.tunw= s2
+    return node
+
+
+@pytest.fixture
+def wg2(wg_keys, wg_ports):
+    (private_1, public_1), (private_2, public_2) = wg_keys
+    (port_1, port_2) = wg_ports
+    node = WgNode(ip='10.0.0.2', listen_ip='127.0.0.1', listen_port=port_2,
+                  private=private_2, public=public_2, peers=[])
+    node.peers.append(WgPeer('10.0.0.1', ('127.0.0.1', port_1), public_1, node))
+    s1, s2 = socket.socketpair(type=socket.SOCK_DGRAM)
+    node.tun = s1.fileno()
+    node._tun = s1
+    node.tunw= s2
+    return node
+
+
+def test_integrate(wg1, wg2):
+    threading.Thread(target=wg1.serve, daemon=True).start()
+    threading.Thread(target=wg2.serve, daemon=True).start()
+
+    def node1_to_node2(data: bytes):
+        packet = IP(src=wg1.ip, dst=wg2.ip, len=len(data)) / data
+        wg1.tunw.send(raw(packet))
+        received_packet = IP(wg2.tunw.recv(65536))
+        assert received_packet.dst == wg2.ip
+        assert received_packet.len == len(data)
+        assert raw(received_packet.payload)[:received_packet.len] == data
+
+    def node2_to_node1(data: bytes):
+        packet = IP(src=wg2.ip, dst=wg1.ip, len=len(data)) / data
+        wg2.tunw.send(raw(packet))
+        received_packet = IP(wg1.tunw.recv(65536))
+        assert received_packet.dst == wg1.ip
+        assert received_packet.len == len(data)
+        assert raw(received_packet.payload)[:received_packet.len] == data
+
+    node1_to_node2(b'hello')
+    node2_to_node1(b'world')
+    assert wg1.peers[0].cur_session.send_count == 1
+    assert wg2.peers[0].cur_session.send_count == 1
+    assert wg1.peers[0].cur_session.max_recv_count == 0
+    assert wg2.peers[0].cur_session.max_recv_count == 0
+    assert wg1.peers[0].cur_session.recv_window == 1
+    assert wg2.peers[0].cur_session.recv_window == 1
+
+
+def test_handshake(wg1, wg2):
+    handshake = wg1.init_handshake(wg2.public)
+    si_pub, timestamp, r_pair, resp_msg = wg2.respond_to_handshake(handshake.req)
     i_pair = handshake.on_response(resp_msg)
     assert r_pair.decrypt_data(i_pair.encrypt_data(b'h'*16)) == b'h'*16
-    assert si_pub == i_public
+    assert si_pub == wg1.public
 
 
 def test_left_padding_int():
@@ -29,7 +113,7 @@ def test_bench_encrypt_decrypt():
                                       send_key=key2, recv_key=key1, establish_time=time.time(), is_initiator=False)
 
     plain = b'h' * 1488
-    loop_count = 100000
+    loop_count = 10000
     elapsed = timeit.timeit('assert recv_keypair.decrypt_data(send_keypair.encrypt_data(plain)) == plain',
                             globals=locals(), number=loop_count)
     rate = (len(plain) * loop_count) / elapsed / 2**20
